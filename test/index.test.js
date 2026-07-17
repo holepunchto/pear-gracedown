38a3432b3450a10f4b6821c67db0a977248d1ccc
Geordan Gesink <165498764+geordangesink@users.noreply.github.com>
2026-07-17 09:18:55 +0200

    fix Signals (#7)
    * fix Signals
    
    * adjust test
    
    * lint
    
    * use varibale

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,7 @@
 'use strict'
 global.Pear = {}
 const { isWindows, isNode, isBare } = require('which-runtime')
+const { spawn } = isBare ? require('bare-subprocess') : require('child_process')
 if (isNode) nodeSetup()
 const run = require('pear-run')
 const { test } = require('brittle')
@@ -12,7 +13,7 @@ if (isBare) os.chdir(__dirname)
 else process.chdir(__dirname)
 
 async function untilResult(pipe, opts = {}) {
-  const timeout = opts.timeout || 10000
+  const { timeout = 10000, runFn, writeStart } = opts
   const res = new Promise((resolve, reject) => {
     let buffer = ''
     const timeoutId = setTimeout(() => reject(new Error('timed out')), timeout)
@@ -28,9 +29,9 @@ async function untilResult(pipe, opts = {}) {
       reject(new Error('unexpected closed'))
     })
   })
-  if (opts.runFn) {
-    await opts.runFn()
-  } else {
+  if (runFn) {
+    await runFn()
+  } else if (writeStart === true) {
     pipe.write('start')
   }
   return res
@@ -67,6 +68,45 @@ test('teardown default', { skip: isWindows }, async function (t) {
   })
   const pipe = run(dir)
 
-  const td = await untilResult(pipe, { runFn: () => pipe.end() })
+  const td = await untilResult(pipe, {
+    runFn: () => pipe.end(),
+    writeStart: true
+  })
   t.is(td, 'teardown', 'teardown executed')
+})
+
+test('teardown on SIGTERM', { skip: isWindows }, async function (t) {
+  t.plan(4)
+  t.timeout(10000)
+
+  const fixture = path.join(__dirname, 'fixtures', 'signal', 'index.js')
+  const child = spawn(program.argv[0], [fixture], {
+    stdio: ['ignore', 'pipe', 'pipe']
+  })
+
+  let stderr = ''
+
+  child.stderr.on('data', (data) => {
+    stderr += data.toString()
+  })
+
+  const closed = new Promise((resolve) => {
+    child.once('close', (code, signal) => resolve({ code, signal }))
+  })
+
+  t.teardown(() => {
+    if (child.exitCode === null) child.kill()
+  })
+
+  t.is(await untilResult(child.stdout), 'ready', 'child ready')
+
+  const cleanup = untilResult(child.stdout)
+  child.kill('SIGTERM')
+
+  const { code, signal } = await closed
+  const status = signal === 'SIGTERM' ? 143 : signal ? 128 + signal : code
+
+  t.is(status, 143, 'exited with SIGTERM')
+  t.is(await cleanup, 'cleanup', 'teardown executed')
+  t.is(stderr, '', 'no signal resend error')
 })
